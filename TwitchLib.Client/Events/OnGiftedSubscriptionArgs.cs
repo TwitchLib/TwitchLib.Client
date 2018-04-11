04e9d1141c6fc4d35f9a8ca6c79e27df565c2c14
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2018-04-11 14:07:58 -0600

    Channel added to subscription args.

--- TwitchLib.Client/Events/OnGiftedSubscriptionArgs.cs
+++ TwitchLib.Client/Events/OnGiftedSubscriptionArgs.cs
@@ -5,6 +5,9 @@ namespace TwitchLib.Client.Events
 {
     public class OnGiftedSubscriptionArgs : EventArgs
     {
+        /// <summary>Property representing the information of the gifted subscription.</summary>
         public GiftedSubscription GiftedSubscription;
+        /// <summary>Property representing the Twitch channel this event fired from.</summary>
+        public string Channel;
     }
 }
