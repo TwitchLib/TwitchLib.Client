0b12d1e9199fc3fb6809486df6d551044ac41bd1
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2018-05-13 20:59:09 -0600

    resolves merge conflicts

--- TwitchLib.Client/Models/OutboundChatMessage.cs
+++ TwitchLib.Client/Models/OutboundChatMessage.cs
@@ -8,13 +8,9 @@
 
         public override string ToString()
         {
-<<<<<<< HEAD
             var user = Username.ToLower();
             var channel = Channel.ToLower();
             return $":{user}!{user}@{user}.tmi.twitch.tv PRIVMSG #{channel} :{Message}";
-=======
-            return $":{Username.ToLower()}!{Username.ToLower()}@{Username.ToLower()}.tmi.twitch.tv PRIVMSG #{Channel.ToLower()} :{Message}";
->>>>>>> aabddb03ffa6068b654852fd31b2e0873875ba79
         }
     }
 }
