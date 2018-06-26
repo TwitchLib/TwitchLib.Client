331139becc1d8a23b049a6bd77030ff28b6ac942
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2018-06-26 15:28:30 -0600

    Removes methods that allowed programmers to use extension methods without providing channel.

--- TwitchLib.Client/Extensions/TimeoutUserExt.cs
+++ TwitchLib.Client/Extensions/TimeoutUserExt.cs
@@ -37,20 +37,6 @@ namespace TwitchLib.Client.Extensions
             if (joinedChannel != null)
                 TimeoutUser(client, joinedChannel, viewer, duration, message, dryRun);
         }
-
-        /// <summary>
-        /// Timesout a user using the first joined channel.
-        /// </summary>
-        /// <param name="viewer">Viewer name to timeout</param>
-        /// <param name="duration">Duration of the timeout via TimeSpan object</param>
-        /// <param name="message">Message to accompany the timeout and show the user.</param>
-        /// <param name="dryRun">Indicates a dryrun (will not sened if true)</param>
-        /// <param name="client">Client reference used to identify extension.</param>
-        public static void TimeoutUser(this ITwitchClient client, string viewer, TimeSpan duration, string message = "", bool dryRun = false)
-        {
-            if (client.JoinedChannels.Count > 0)
-                TimeoutUser(client, client.JoinedChannels[0], viewer, duration, message, dryRun);
-        }
         #endregion
     }
 }
