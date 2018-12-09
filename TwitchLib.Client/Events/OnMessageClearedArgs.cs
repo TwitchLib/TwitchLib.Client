2c2b0e3f0e40c54dafc95c0acd6510a1fa9bd323
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2018-12-08 16:56:23 -0800

    Support for detecting removed messages

---
+++ TwitchLib.Client/Events/OnMessageClearedArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitchLib.Client.Events
+{
+    /// <inheritdoc />
+    /// <summary>Args representing a cleared message event.</summary>
+    public class OnMessageClearedArgs : EventArgs
+    {
+        /// <summary>Channel that had message cleared event.</summary>
+        public string Channel;
+
+        /// <summary>Message contents that received clear message</summary>
+        public string Message;
+    }
+}
