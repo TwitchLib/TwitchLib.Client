80d15168c4a64c1aa859d1f369ab357ff3f2c3ef
Peter Malík <p.malik@malikp.eu>
2019-04-27 00:06:20 +0200

    Add 'UserTimeout' builder

---
+++ TwitchLib.Client.Models/Builders/UserTimeoutBuilder.cs
@@ -0,0 +1,55 @@
+﻿using TwitchLib.Client.Models.Internal;
+
+namespace TwitchLib.Client.Models.Builders
+{
+    public sealed class UserTimeoutBuilder : IBuilder<UserTimeout>
+    {
+        private string _channel;
+        private int _timeoutDuration;
+        private string _timeoutReason;
+        private string _username;
+
+        private UserTimeoutBuilder()
+        {
+        }
+
+        public UserTimeoutBuilder WithChannel(string channel)
+        {
+            _channel = channel;
+            return this;
+        }
+
+        public UserTimeoutBuilder WithTimeoutDuration(int timeoutDuration)
+        {
+            _timeoutDuration = timeoutDuration;
+            return this;
+        }
+
+        public UserTimeoutBuilder WithTimeoutReason(string timeoutReason)
+        {
+            _timeoutReason = timeoutReason;
+            return this;
+        }
+
+        public UserTimeoutBuilder WithUsername(string username)
+        {
+            _username = username;
+            return this;
+        }
+
+        public static UserTimeoutBuilder Create()
+        {
+            return new UserTimeoutBuilder();
+        }
+
+        public UserTimeout BuildFromIrcMessage(IrcMessage ircMessage)
+        {
+            return new UserTimeout(ircMessage);
+        }
+
+        public UserTimeout Build()
+        {
+            return new UserTimeout(_channel, _username, _timeoutDuration, _timeoutReason);
+        }
+    }
+}
