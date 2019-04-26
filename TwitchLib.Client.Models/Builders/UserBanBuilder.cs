b90cbee84fe898fa37ad34a4c7ae7fe3083b8808
Peter Malík <p.malik@malikp.eu>
2019-04-26 22:50:24 +0200

    Add 'UserBan' builder

---
+++ TwitchLib.Client.Models/Builders/UserBanBuilder.cs
@@ -0,0 +1,48 @@
+﻿using TwitchLib.Client.Models.Internal;
+
+namespace TwitchLib.Client.Models.Builders
+{
+    public sealed class UserBanBuilder : IBuilder<UserBan>
+    {
+        private string _channelName;
+        private string _userName;
+        private string _banReason;
+
+        public static UserBanBuilder Create()
+        {
+            return new UserBanBuilder();
+        }
+
+        private UserBanBuilder()
+        {
+        }
+
+        public UserBanBuilder WithChannel(string channel)
+        {
+            _channelName = channel;
+            return this;
+        }
+
+        public UserBanBuilder WithUserName(string userName)
+        {
+            _userName = userName;
+            return this;
+        }
+
+        public UserBanBuilder WithBanReason(string banReason)
+        {
+            _banReason = banReason;
+            return this;
+        }
+
+        public UserBan BuildFromIrcMessage(IrcMessage ircMessage)
+        {
+            return new UserBan(ircMessage);
+        }
+
+        public UserBan Build()
+        {
+            return new UserBan(_channelName, _userName, _banReason);
+        }
+    }
+}
