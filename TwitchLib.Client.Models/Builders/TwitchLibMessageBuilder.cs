624eeb2f99d2c3f6c066858e41a65b395e3e5b0e
Peter Malík <p.malik@malikp.eu>
2019-04-26 23:27:47 +0200

    Add initial version of TwitchLibMessageBuilder

---
+++ TwitchLib.Client.Models/Builders/TwitchLibMessageBuilder.cs
@@ -0,0 +1,26 @@
+﻿namespace TwitchLib.Client.Models.Builders
+{
+    public sealed class TwitchLibMessageBuilder : IBuilder<TwitchLibMessage>
+    {
+        private readonly TempTwitchLibMessage _message = new TempTwitchLibMessage();
+
+        private TwitchLibMessageBuilder()
+        {
+            // TODO: Add with methods
+        }
+
+        public static TwitchLibMessageBuilder Create()
+        {
+            return new TwitchLibMessageBuilder();
+        }
+
+        public TwitchLibMessage Build()
+        {
+            return _message;
+        }
+
+        private class TempTwitchLibMessage : TwitchLibMessage
+        {
+        }
+    }
+}
