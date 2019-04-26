63b7fbf9ee2f16dc003f36ef76cb44eda417e460
Peter Malík <p.malik@malikp.eu>
2019-04-26 23:14:05 +0200

    Reformat document for better readibility - TwitchLibMessage

--- TwitchLib.Client.Models/TwitchLibMessage.cs
+++ TwitchLib.Client.Models/TwitchLibMessage.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
+
 using TwitchLib.Client.Enums;
 
 
@@ -9,24 +10,33 @@ namespace TwitchLib.Client.Models
     public abstract class TwitchLibMessage
     {
         /// <summary>List of key-value pair badges.</summary>
-        public List<KeyValuePair<string, string>> Badges { get; protected set;}
+        public List<KeyValuePair<string, string>> Badges { get; protected set; }
+
         /// <summary>Twitch username of the bot that received the message.</summary>
-        public string BotUsername { get; protected set;}
+        public string BotUsername { get; protected set; }
+
         /// <summary>Property representing HEX color as a System.Drawing.Color object.</summary>
-        public Color Color { get; protected set;}
+        public Color Color { get; protected set; }
+
         /// <summary>Hex representation of username color in chat (THIS CAN BE NULL IF VIEWER HASN'T SET COLOR).</summary>
-        public string ColorHex { get; protected set;}
+        public string ColorHex { get; protected set; }
+
         /// <summary>Case-sensitive username of sender of chat message.</summary>
-        public string DisplayName { get; protected set;}
+        public string DisplayName { get; protected set; }
+
         /// <summary>Emote Ids that exist in message.</summary>
-        public EmoteSet EmoteSet { get; protected set;}
+        public EmoteSet EmoteSet { get; protected set; }
+
         /// <summary>Twitch site-wide turbo status.</summary>
-        public bool IsTurbo { get; protected set;}
+        public bool IsTurbo { get; protected set; }
+
         /// <summary>Twitch-unique integer assigned on per account basis.</summary>
-        public string UserId { get; protected set;}
+        public string UserId { get; protected set; }
+
         /// <summary>Username of sender of chat message.</summary>
-        public string Username { get; protected set;}
+        public string Username { get; protected set; }
+
         /// <summary>User type can be viewer, moderator, global mod, admin, or staff</summary>
-        public UserType UserType { get; protected set;}
+        public UserType UserType { get; protected set; }
     }
 }
