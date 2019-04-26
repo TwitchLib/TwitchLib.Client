7f22e3390e7c1787c9d4780530a3577c16aed310
Peter Malík <p.malik@malikp.eu>
2019-04-26 23:47:32 +0200

    Finalize 'TwitchLibMessageBuilder' - add with* methods

--- TwitchLib.Client.Models/Builders/TwitchLibMessageBuilder.cs
+++ TwitchLib.Client.Models/Builders/TwitchLibMessageBuilder.cs
@@ -1,12 +1,74 @@
-﻿namespace TwitchLib.Client.Models.Builders
-{
-    public sealed class TwitchLibMessageBuilder : IBuilder<TwitchLibMessage>
-    {
-        private readonly TempTwitchLibMessage _message = new TempTwitchLibMessage();
+﻿using System.Collections.Generic;
+using System.Drawing;
 
+using TwitchLib.Client.Enums;
+
+namespace TwitchLib.Client.Models.Builders
+{
+    public sealed class TwitchLibMessageBuilder : TwitchLibMessage, IBuilder<TwitchLibMessage>
+    {
         private TwitchLibMessageBuilder()
         {
-            // TODO: Add with methods
+        }
+
+        public TwitchLibMessageBuilder WithBadges(List<KeyValuePair<string, string>> badges)
+        {
+            Badges = badges;
+            return this;
+        }
+
+        public TwitchLibMessageBuilder WithColorHex(string colorHex)
+        {
+            ColorHex = colorHex;
+            return this;
+        }
+
+        public TwitchLibMessageBuilder WithColorHex(Color color)
+        {
+            Color = color;
+            return this;
+        }
+
+        public TwitchLibMessageBuilder WithUsername(string username)
+        {
+            Username = username;
+            return this;
+        }
+
+        public TwitchLibMessageBuilder WithDisplayName(string displayName)
+        {
+            DisplayName = displayName;
+            return this;
+        }
+
+        public TwitchLibMessageBuilder WithEmoteSet(EmoteSet emoteSet)
+        {
+            EmoteSet = emoteSet;
+            return this;
+        }
+
+        public TwitchLibMessageBuilder WithUserId(string userId)
+        {
+            UserId = userId;
+            return this;
+        }
+
+        public TwitchLibMessageBuilder WithIsTurbo(bool isTurbo)
+        {
+            IsTurbo = isTurbo;
+            return this;
+        }
+
+        public TwitchLibMessageBuilder WithBotUserName(string botUserName)
+        {
+            BotUsername = botUserName;
+            return this;
+        }
+
+        public TwitchLibMessageBuilder WithUserType(UserType userType)
+        {
+            UserType = userType;
+            return this;
         }
 
         public static TwitchLibMessageBuilder Create()
@@ -16,11 +78,7 @@
 
         public TwitchLibMessage Build()
         {
-            return _message;
-        }
-
-        private class TempTwitchLibMessage : TwitchLibMessage
-        {
+            return this;
         }
     }
 }
