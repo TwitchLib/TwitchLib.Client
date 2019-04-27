3a2a4473a77da091b7f711763f9a141e2b8b1b36
Peter Malík <p.malik@malikp.eu>
2019-04-27 09:44:30 +0200

    Add interface 'FromIrcmessageBuilder'

---
+++ TwitchLib.Client.Models/Builders/IFromIrcMessageBuilder.cs
@@ -0,0 +1,9 @@
+﻿using TwitchLib.Client.Models.Internal;
+
+namespace TwitchLib.Client.Models.Builders
+{
+    public interface IFromIrcMessageBuilder<T>
+    {
+        T BuildFromIrcMessage(IrcMessage ircMessage);
+    }
+}
