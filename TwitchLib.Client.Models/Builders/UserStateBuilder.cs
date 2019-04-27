cad3a4fa71215bcdb4109bfd3aa8a9fa3d027846
Peter Malík <p.malik@malikp.eu>
2019-04-27 08:20:54 +0200

    Add 'UserState' builder

---
+++ TwitchLib.Client.Models/Builders/UserStateBuilder.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+
+using TwitchLib.Client.Enums;
+using TwitchLib.Client.Models.Internal;
+
+namespace TwitchLib.Client.Models.Builders
+{
+    public sealed class UserStateBuilder : IBuilder<UserState>
+    {
+        private List<KeyValuePair<string, string>> _badges = new List<KeyValuePair<string, string>>();
+        private string _channel;
+        private string _colorHex;
+        private string _displayName;
+        private string _emoteSet;
+        private bool _isModerator;
+        private bool _isSubscriber;
+        private UserType _userType;
+
+        private UserStateBuilder()
+        {
+        }
+
+        public UserStateBuilder WithBadges(params KeyValuePair<string, string>[] pairs)
+        {
+            _badges.AddRange(_badges);
+            return this;
+        }
+
+        public UserStateBuilder WithChannel(string channel)
+        {
+            _channel = channel;
+            return this;
+        }
+
+        public UserStateBuilder WithColorHex(string olorHex)
+        {
+            _colorHex = olorHex;
+            return this;
+        }
+
+        public UserStateBuilder WithDisplayName(string displayName)
+        {
+            _displayName = displayName;
+            return this;
+        }
+
+        public UserStateBuilder WithEmoteSet(string emoteSet)
+        {
+            _emoteSet = emoteSet;
+            return this;
+        }
+
+        public UserStateBuilder WithIsModerator(bool isModerator)
+        {
+            _isModerator = isModerator;
+            return this;
+        }
+
+        public UserStateBuilder WithIsSubscriber(bool isSubscriber)
+        {
+            _isSubscriber = isSubscriber;
+            return this;
+        }
+
+        public UserStateBuilder WithUserType(UserType userType)
+        {
+            _userType = userType;
+            return this;
+        }
+
+        public static UserStateBuilder Create()
+        {
+            return new UserStateBuilder();
+        }
+
+        public UserState BuildFromIrcMessage(IrcMessage ircMessage)
+        {
+            return new UserState(ircMessage);
+        }
+
+        public UserState Build()
+        {
+            return new UserState(
+                _badges,
+                _colorHex,
+                _displayName,
+                _emoteSet,
+                _channel,
+                _isSubscriber,
+                _isModerator,
+                _userType);
+        }
+    }
+}
