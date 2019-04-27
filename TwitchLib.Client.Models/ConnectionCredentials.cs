eb6b89c37e7417082c9f6c6ca75e3ef3ad742bcb
Peter Malík <p.malik@malikp.eu>
2019-04-27 10:29:53 +0200

    Reformat document for better readibility

--- TwitchLib.Client.Models/ConnectionCredentials.cs
+++ TwitchLib.Client.Models/ConnectionCredentials.cs
@@ -6,6 +6,8 @@ namespace TwitchLib.Client.Models
     /// <summary>Class used to store credentials used to connect to Twitch chat/whisper.</summary>
     public class ConnectionCredentials
     {
+        public const string DefaultWebSocketUri = "wss://irc-ws.chat.twitch.tv:443";
+
         /// <summary>Property representing URI used to connect to Twitch websocket service.</summary>
         public string TwitchWebsocketURI { get; }
 
@@ -16,17 +18,24 @@ namespace TwitchLib.Client.Models
         public string TwitchUsername { get; }
 
         /// <summary>Constructor for ConnectionCredentials object.</summary>
-        public ConnectionCredentials(string twitchUsername, string twitchOAuth, string twitchWebsocketURI = "wss://irc-ws.chat.twitch.tv:443", bool disableUsernameCheck = false)
+        public ConnectionCredentials(
+            string twitchUsername,
+            string twitchOAuth,
+            string twitchWebsocketURI = DefaultWebSocketUri,
+            bool disableUsernameCheck = false)
         {
             if (!disableUsernameCheck && !new Regex("^([a-zA-Z0-9][a-zA-Z0-9_]{3,25})$").Match(twitchUsername).Success)
                 throw new Exception($"Twitch username does not appear to be valid. {twitchUsername}");
+
             TwitchUsername = twitchUsername.ToLower();
             TwitchOAuth = twitchOAuth;
+
             // Make sure proper formatting is applied to oauth
             if (!twitchOAuth.Contains(":"))
             {
                 TwitchOAuth = $"oauth:{twitchOAuth.Replace("oauth", "")}";
             }
+
             TwitchWebsocketURI = twitchWebsocketURI;
         }
     }
