7f914d54b4413019417790863d14e598da86c970
Peter Malík <p.malik@malikp.eu>
2019-04-28 21:41:49 +0200

    Finalize 'ChatMessage' builder

--- TwitchLib.Client.Models/Builders/ChatMessageBuilder.cs
+++ TwitchLib.Client.Models/Builders/ChatMessageBuilder.cs
@@ -7,7 +7,7 @@ namespace TwitchLib.Client.Models.Builders
     public sealed class ChatMessageBuilder : IBuilder<ChatMessage>
     {
         private TwitchLibMessage _twitchLibMessage;
-        private List<KeyValuePair<string, string>> BadgeInfo = new List<KeyValuePair<string, string>>();
+        private readonly List<KeyValuePair<string, string>> BadgeInfo = new List<KeyValuePair<string, string>>();
         private int _bits;
         private double _bitsInDollars;
         private string _channel;
@@ -26,7 +26,109 @@ namespace TwitchLib.Client.Models.Builders
 
         private ChatMessageBuilder()
         {
-            // todo: add with* methods
+            _twitchLibMessage = TwitchLibMessageBuilder.Create().Build();
+        }
+
+        public ChatMessageBuilder WithTwitchLibMessage(TwitchLibMessage twitchLibMessage)
+        {
+            _twitchLibMessage = twitchLibMessage;
+            return this;
+        }
+
+        public ChatMessageBuilder WithBadgeInfos(params KeyValuePair<string, string>[] badgeInfos)
+        {
+            BadgeInfo.AddRange(badgeInfos);
+            return this;
+        }
+
+        public ChatMessageBuilder WithBits(int bits)
+        {
+            _bits = bits;
+            return this;
+        }
+
+        public ChatMessageBuilder WithBitsInDollars(double bitsInDollars)
+        {
+            _bitsInDollars = bitsInDollars;
+            return this;
+        }
+
+        public ChatMessageBuilder WithChannel(string channel)
+        {
+            _channel = channel;
+            return this;
+        }
+
+        public ChatMessageBuilder WithCheerBadge(CheerBadge cheerBadge)
+        {
+            _cheerBadge = cheerBadge;
+            return this;
+        }
+
+        public ChatMessageBuilder WithEmoteReplaceMessage(string emoteReplaceMessage)
+        {
+            _emoteReplacedMessage = emoteReplaceMessage;
+            return this;
+        }
+
+        public ChatMessageBuilder WithId(string id)
+        {
+            _id = id;
+            return this;
+        }
+
+        public ChatMessageBuilder WithIsBroadcaster(bool isBroadcaster)
+        {
+            _isBroadcaster = isBroadcaster;
+            return this;
+        }
+
+        public ChatMessageBuilder WithIsMe(bool isMe)
+        {
+            _isMe = isMe;
+            return this;
+        }
+
+        public ChatMessageBuilder WithIsModerator(bool isModerator)
+        {
+            _isModerator = isModerator;
+            return this;
+        }
+
+        public ChatMessageBuilder WithIsSubscriber(bool isSubscriber)
+        {
+            _isSubscriber = isSubscriber;
+            return this;
+        }
+
+        public ChatMessageBuilder WithMessage(string message)
+        {
+            _message = message;
+            return this;
+        }
+
+        public ChatMessageBuilder WithNoisy(Noisy noisy)
+        {
+            _noisy = noisy;
+            return this;
+        }
+
+        public ChatMessageBuilder WithRawIrcMessage(string rawIrcMessage)
+        {
+            _rawIrcMessage = rawIrcMessage;
+            return this;
+        }
+
+        public ChatMessageBuilder WithRoomId(string roomId)
+        {
+            _roomId = roomId;
+            return this;
+        }
+
+        public ChatMessageBuilder WithSubscribedMonthCount(int subscribedMonthCount)
+        {
+            _subscribedMonthCount = subscribedMonthCount;
+            return this;
         }
 
         public static ChatMessageBuilder Create()
