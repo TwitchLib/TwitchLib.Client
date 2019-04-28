6c5669c23c4a104be6ffe16eb4b914fdc2fb9173
Peter Malík <p.malik@malikp.eu>
2019-04-28 23:22:48 +0200

    Add 'ErrorEvent' builder - there is no need for this builder but this will fit that all model classes has own builder and also willsupport to easily extend and add params to ctor.

---
+++ TwitchLib.Client.Models/Builders/ErrorEventBuilder.cs
@@ -0,0 +1,24 @@
+﻿namespace TwitchLib.Client.Models.Builders
+{
+    public sealed class ErrorEventBuilder : IBuilder<ErrorEvent>
+    {
+        private string _message;
+
+        private ErrorEventBuilder()
+        {
+        }
+
+        public static ErrorEventBuilder Create()
+        {
+            return new ErrorEventBuilder();
+        }
+
+        public ErrorEvent Build()
+        {
+            return new ErrorEvent
+            {
+                Message = _message
+            };
+        }
+    }
+}
