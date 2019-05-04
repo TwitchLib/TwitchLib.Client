7936c203ff9afb380fe8f8af163a700837752bd6
Peter Malík <p.malik@malikp.eu>
2019-05-04 18:24:21 +0200

    Fix event builder

--- TwitchLib.Client.Models/Builders/ErrorEventBuilder.cs
+++ TwitchLib.Client.Models/Builders/ErrorEventBuilder.cs
@@ -8,6 +8,12 @@
         {
         }
 
+        public ErrorEventBuilder WithMessage(string message)
+        {
+            _message = message;
+            return this;
+        }
+
         public static ErrorEventBuilder Create()
         {
             return new ErrorEventBuilder();
