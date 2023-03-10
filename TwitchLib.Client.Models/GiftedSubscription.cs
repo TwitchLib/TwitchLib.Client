b2b2847ef84b6693a83cd88a3bc519665e31cb9c
CMR <github.christian-riedel@outlook.de>
2023-03-10 07:50:40 +0100

    TODO: rather logging than throwing an exception

--- TwitchLib.Client.Models/GiftedSubscription.cs
+++ TwitchLib.Client.Models/GiftedSubscription.cs
@@ -128,7 +128,9 @@ namespace TwitchLib.Client.Models
                             case "":
                                 break;
                             default:
-                                throw new ArgumentOutOfRangeException(nameof(tagValue.ToLower));
+                                // TODO: rather logging than throwing an exception
+                                Exception ex = new ArgumentOutOfRangeException(nameof(tagValue));
+                                break;
                         }
                         break;
                     case Tags.RoomId:
