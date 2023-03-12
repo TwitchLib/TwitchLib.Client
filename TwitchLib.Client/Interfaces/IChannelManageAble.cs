4f260ba10a012c620207482837475dd1f1cff7e4
CMR <github.christian-riedel@outlook.de>
2023-03-12 13:59:26 +0100

    to ensure ChannelManager and TwitchClient provide the same methods

---
+++ TwitchLib.Client/Interfaces/IChannelManageAble.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+using TwitchLib.Client.Models;
+
+namespace TwitchLib.Client.Interfaces
+{
+    public interface IChannelManageAble
+    {
+        /// <summary>
+        ///     <see cref="IReadOnlyList{T}"/>
+        /// </summary>
+        IReadOnlyList<JoinedChannel> JoinedChannels { get; }
+        /// <summary>
+        ///     enqueues the given <paramref name="channel"/> to get joined
+        /// </summary>
+        /// <param name="channel">
+        ///     name of the channel to join
+        /// </param>
+        /// <param name="overrideCheck">
+        /// </param>
+        void JoinChannel(string channel, bool overrideCheck = false);
+        /// <summary>
+        ///     returns the <see cref="JoinedChannel"/> of the channel with the given <paramref name="channel"/>-name
+        ///     <br></br>
+        ///     <br></br>
+        ///     <see langword="null"/> if its (still) not joined
+        /// </summary>
+        /// <param name="channel">
+        ///     name of the channel
+        /// </param>
+        /// <returns>
+        ///     <see cref="JoinedChannel"/> if the channel with the given <paramref name="channel"/>-name is joined
+        ///     <br></br>
+        ///     <br></br>
+        ///     <see langword="null"/> otherwise (if its (still) not joined)
+        /// </returns>
+        JoinedChannel GetJoinedChannel(string channel);
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <param name="channel">
+        ///     name of the channel to leave
+        /// </param>
+        void LeaveChannel(string channel);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="channel">
+        ///     <see cref="JoinedChannel"/> to leave
+        /// </param>
+        void LeaveChannel(JoinedChannel channel);
+
+    }
+}
--- TwitchLib.Client/Interfaces/ITwitchClient_Channel.cs
+++ TwitchLib.Client/Interfaces/ITwitchClient_Channel.cs
@@ -1,20 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 
 using TwitchLib.Client.Events;
-using TwitchLib.Client.Models;
 
 namespace TwitchLib.Client.Interfaces
 {
     /// <summary>
     ///     everthing related to <see cref="Models.JoinedChannel"/>s
     /// </summary>
-    public interface ITwitchClient_Channel
+    public interface ITwitchClient_Channel : IChannelManageAble
     {
-        /// <summary>
-        ///     <see cref="Models.JoinedChannel"/>
-        /// </summary>
-        IReadOnlyList<JoinedChannel> JoinedChannels { get; }
 
         /// <summary>
         ///     Occurs when [on channel state changed].
@@ -32,40 +26,5 @@ namespace TwitchLib.Client.Interfaces
         ///     Occurs when [on left channel].
         /// </summary>
         event EventHandler<OnLeftChannelArgs> OnLeftChannel;
-
-        /// <summary>
-        /// </summary>
-        /// <param name="channel">
-        ///     name of the channel
-        /// </param>
-        /// <returns>
-        ///     <see cref="Models.JoinedChannel"/>
-        /// </returns>
-        JoinedChannel GetJoinedChannel(string channel);
-
-        /// <summary>
-        /// </summary>
-        /// <param name="channel">
-        ///     The channel.
-        /// </param>
-        /// <param name="overrideCheck">
-        ///     if set to <see langword="true"/> [override check].
-        /// </param>
-        void JoinChannel(string channel, bool overrideCheck = false);
-        /// <summary>
-        /// </summary>
-        /// <param name="channel">
-        ///     <see cref="Models.JoinedChannel"/>
-        ///     <br></br>
-        ///     <br></br>
-        ///     <seealso cref="GetJoinedChannel(String)"/>
-        /// </param>
-        void LeaveChannel(JoinedChannel channel);
-        /// <summary>
-        /// </summary>
-        /// <param name="channel">
-        ///     The channel.
-        /// </param>
-        void LeaveChannel(string channel);
     }
 }
