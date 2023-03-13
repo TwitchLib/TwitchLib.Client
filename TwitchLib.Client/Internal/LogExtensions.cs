5bcaf1d4b7872061184989742ad55f74c8be1bbd
CMR <github.christian-riedel@outlook.de>
2023-03-13 05:24:42 +0100

    LogExtensions

---
+++ TwitchLib.Client/Internal/LogExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+using Microsoft.Extensions.Logging;
+
+namespace TwitchLib.Communication.Extensions
+{
+    /// <summary>
+    ///     expensive Extensions of the <see cref="ILogger"/>
+    /// </summary>
+    internal static class LogExtensions
+    {
+        public static void TraceMethodCall(this ILogger logger,
+                                           Type type,
+                                           [CallerMemberName] string callerMemberName = "",
+                                           [CallerLineNumber] int callerLineNumber = 0)
+        {
+            // because of the code-formatting, 2 line is subtracted from the callerLineNumber
+            // cant be done inline!
+            callerLineNumber -= 2;
+            logger?.LogTrace("{FullName}.{callerMemberName} at line {callerLineNumber} is called",
+                             type.FullName, callerMemberName, callerLineNumber);
+        }
+        public static void LogExceptionAsError(this ILogger logger,
+                                               Type type,
+                                               Exception exception,
+                                               [CallerMemberName] string callerMemberName = "",
+                                               [CallerLineNumber] int callerLineNumber = 0)
+        {
+            logger?.LogError(exception,
+                             "Exception in {FullName}.{callerMemberName} at line {callerLineNumber}:",
+                             type.FullName, callerMemberName, callerLineNumber);
+        }
+        public static void LogExceptionAsInformation(this ILogger logger,
+                                                     Type type,
+                                                     Exception exception,
+                                                     [CallerMemberName] string callerMemberName = "",
+                                                     [CallerLineNumber] int callerLineNumber = 0)
+        {
+            logger?.LogInformation(exception,
+                                   "Exception in {FullName}.{callerMemberName} at line {callerLineNumber}:",
+                                   type.FullName, callerMemberName, callerLineNumber);
+        }
+        public static void TraceAction(this ILogger logger,
+                                       Type type,
+                                       string action,
+                                       [CallerMemberName] string callerMemberName = "",
+                                       [CallerLineNumber] int callerLineNumber = 0)
+        {
+            logger?.LogTrace("{FullName}.{callerMemberName} at line {callerLineNumber}: {action}",
+                             type.FullName, callerMemberName, callerLineNumber, action);
+        }
+    }
+}
