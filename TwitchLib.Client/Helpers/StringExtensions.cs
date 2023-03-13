9502f451579e82e2ad2866fea2b4fe031d839a1c
CMR <github.christian-riedel@outlook.de>
2023-03-13 06:31:27 +0100

    string extensions

---
+++ TwitchLib.Client/Helpers/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TwitchLib.Client.Helpers
+{
+    internal static class StringExtensions
+    {
+        public static bool IsNullOrEmptyOrWhitespace(this string s)
+        {
+            return String.IsNullOrEmpty(s) || String.IsNullOrWhiteSpace(s);
+        }
+    }
+}
