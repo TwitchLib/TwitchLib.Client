6e4556feac9c731dd472ca8734700c222ca70747
CMR <github.christian-riedel@outlook.de>
2023-03-18 10:41:30 +0100

    docs, etsy

--- TwitchLib.Client.Tests/ATwitchClientTests.cs
+++ TwitchLib.Client.Tests/ATwitchClientTests.cs
@@ -1,12 +1,30 @@
-﻿namespace TwitchLib.Client.Tests
+﻿using TwitchLib.Client.Events.Abstracts;
+
+using Xunit;
+
+namespace TwitchLib.Client.Tests
 {
     public abstract class ATwitchClientTests<T>
     {
-        protected static string TWITCH_Username => "Username";
-        protected static string TWITCH_UsernameAnother => "UsernameAnother";
+        /// <summary>
+        ///     has to lower-case
+        /// </summary>
+        protected static string TWITCH_Username => "username";
+        /// <summary>
+        ///     has to lower-case
+        /// </summary>
+        protected static string TWITCH_UsernameAnother => "username_another";
         protected static string TWITCH_OAuth => "OAuth";
+        /// <summary>
+        ///     has to lower-case
+        /// </summary>
         protected static string TWITCH_CHANNEL => "testchannel";
         protected static int WaitOneDuration => 5_000;
         protected static int WaitOneDurationShort => 500;
+
+        protected void AssertChannel(AChannelProvidingEventArgs eventArgs)
+        {
+            Assert.Equal(TWITCH_CHANNEL, eventArgs.Channel);
+        }
     }
 }
