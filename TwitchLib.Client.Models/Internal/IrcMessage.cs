85f1ffaacf4658578da851ff32be927a0da8c840
CMR <github.christian-riedel@outlook.de>
2023-03-20 08:17:07 +0100

    from random variables to properties

--- TwitchLib.Client.Models/Internal/IrcMessage.cs
+++ TwitchLib.Client.Models/Internal/IrcMessage.cs
@@ -13,39 +13,39 @@ namespace TwitchLib.Client.Models.Internal
         /// </summary>
         public string Channel => Params.StartsWith("#") ? Params.Remove(0, 1) : Params;
 
-        public string Params => _parameters != null && _parameters.Length > 0 ? _parameters[0] : "";
+        public string Params => Parameters != null && Parameters.Length > 0 ? Parameters[0] : "";
 
         /// <summary>
         /// Message itself
         /// </summary>
         public string Message => Trailing;
 
-        public string Trailing => _parameters != null && _parameters.Length > 1 ? _parameters[_parameters.Length - 1] : "";
+        public string Trailing => Parameters != null && Parameters.Length > 1 ? Parameters[Parameters.Length - 1] : "";
 
         /// <summary>
         /// Command parameters
         /// </summary>
-        private readonly string[] _parameters;
+        private string[] Parameters { get; }
 
         /// <summary>
         /// The user whose message it is
         /// </summary>
-        public readonly string User;
+        public string User { get; set; }
 
         /// <summary>
         /// Hostmask of the user
         /// </summary>
-        public readonly string Hostmask;
+        public string Hostmask { get; }
 
         /// <summary>
         /// Raw Command
         /// </summary>
-        public readonly IrcCommand Command;
+        public IrcCommand Command { get; }
 
         /// <summary>
         /// IRCv3 tags
         /// </summary>
-        public readonly Dictionary<string, string> Tags;
+        public Dictionary<string, string> Tags { get; }
 
         /// <summary>
         /// Create an INCOMPLETE IrcMessage only carrying username
@@ -53,7 +53,7 @@ namespace TwitchLib.Client.Models.Internal
         /// <param name="user"></param>
         public IrcMessage(string user)
         {
-            _parameters = null;
+            Parameters = null;
             User = user;
             Hostmask = null;
             Command = IrcCommand.Unknown;
@@ -76,7 +76,7 @@ namespace TwitchLib.Client.Models.Internal
             int idx = hostmask.IndexOf('!');
             User = idx != -1 ? hostmask.Substring(0, idx) : hostmask;
             Hostmask = hostmask;
-            _parameters = parameters;
+            Parameters = parameters;
             Command = command;
             Tags = tags;
 
@@ -84,7 +84,7 @@ namespace TwitchLib.Client.Models.Internal
             {
                 if (Params.Length > 0 && Params.Contains("#"))
                 {
-                    _parameters[0] = $"#{_parameters[0].Split('#')[1]}";
+                    Parameters[0] = $"#{Parameters[0].Split('#')[1]}";
                 }
             }
         }
@@ -116,17 +116,17 @@ namespace TwitchLib.Client.Models.Internal
             }
 
             raw.Append(Command.ToString().ToUpper().Replace("RPL_", ""));
-            if (_parameters.Length <= 0)
+            if (Parameters.Length <= 0)
                 return raw.ToString();
 
-            if (_parameters[0] != null && _parameters[0].Length > 0)
+            if (Parameters[0] != null && Parameters[0].Length > 0)
             {
-                raw.Append(" ").Append(_parameters[0]);
+                raw.Append(" ").Append(Parameters[0]);
             }
 
-            if (_parameters.Length > 1 && _parameters[1] != null && _parameters[1].Length > 0)
+            if (Parameters.Length > 1 && Parameters[1] != null && Parameters[1].Length > 0)
             {
-                raw.Append(" :").Append(_parameters[1]);
+                raw.Append(" :").Append(Parameters[1]);
             }
 
             return raw.ToString();
