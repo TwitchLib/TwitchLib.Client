f3f8d1414aceb9cfd588f86a08a2554a15ed3d95
CMR <github.christian-riedel@outlook.de>
2023-03-22 17:14:51 +0100

    (actually) unused IrcJsonParser, perhaps, idk, who knows..

---
+++ TwitchLib.Client.Tests/Parsers/IrcJsonParserTests.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+using Newtonsoft.Json.Linq;
+
+using TwitchLib.Client.Extensions.Internal;
+using TwitchLib.Client.Parsers;
+
+using Xunit;
+
+namespace TwitchLib.Client.Tests.Parsers;
+
+public class IrcJsonParserTests
+{
+    [Theory]
+    // --------------CMD-----USER-------CH---TMS--TAGS--META-------------------MSG
+    [InlineData("001", "testuser", null, true, "", "tmi.twitch.tv {0} {1}", "Welcome, GLHF!")]
+    [InlineData("002", "testuser", null, true, "", "tmi.twitch.tv {0} {1}", "Your host is tmi.twitch.tv")]
+    [InlineData("003", "testuser", null, true, "", "tmi.twitch.tv {0} {1}", "This server is rather new")]
+    [InlineData("004", "testuser", null, true, "", "tmi.twitch.tv {0} {1}", "-")]
+    [InlineData("372", "testuser", null, true, "", "tmi.twitch.tv {0} {1}", "You are in a maze of twisty passages, all alike.")]
+    [InlineData("375", "testuser", null, true, "", "tmi.twitch.tv {0} {1}", "-")]
+    [InlineData("376", "testuser", null, true, "", "tmi.twitch.tv {0} {1}", ">")]
+    //
+    // --------------CMD---USER--CH--TMS--TAGS--META------------------------MSG
+    [InlineData("CAP", null, null, true, "", "tmi.twitch.tv {0} * ACK", "twitch.tv/membership")]
+    [InlineData("NOTICE", null, null, true, "", "tmi.twitch.tv {0} {2}", "Login authentication failed")]
+    //
+    // --------------CMD-------USER---------CH---------TMS--TAGS----META-------------------------------MSG
+    [InlineData("353", "testuser", "testchannel", true, "", "{1}.tmi.twitch.tv {0} {1} = #{2}", "usera userb")]
+    [InlineData("366", "testuser", "testchannel", true, "", "{1}.tmi.twitch.tv {0} {1} #{2}", "End of /NAMES list")]
+    //
+    // --------------CMD-------USER---------CH----------TMS--TAGS----META-------------------------------MSG
+    [InlineData("JOIN", "testuser", "testchannel", true, "", "{1}!{1}@{1}.tmi.twitch.tv {0} #{2}", null)]
+    [InlineData("JOIN", "testuser", "testchannel", true, "", "{1}!{1}@{1}.tmi.twitch.tv {0} #{2}", "another_user")]
+    [InlineData("PART", "testuser", "testchannel", true, "", "{1}!{1}@{1}.tmi.twitch.tv {0} #{2}", null)]
+    [InlineData("PART", "testuser", "testchannel", true, "", "{1}!{1}@{1}.tmi.twitch.tv {0} #{2}", "another_user")]
+    // ping-pong
+    // --------------CMD----USER-CH---TMS--TAGS--META---MSG
+    [InlineData("PING", null, null, false, "", "{0}", "tmi.twitch.tv")]
+    [InlineData("PONG", null, null, false, "", "{0}", "tmi.twitch.tv")]
+    //
+    [InlineData("SERVERCHANGE", null, null, true, "", "tmi.twitch.tv {0}", null)]
+    [InlineData("RECONNECT", null, null, true, "", "tmi.twitch.tv {0}", null)]
+    //
+    // timeout
+    // --------------CMD-------------USER----------CH--------TMS----TAGS-------------------------------------------------------------META------------------MSG
+    [InlineData("CLEARCHAT", "testuser", "testchannel", true, "@ban-duration=60;room-id=0;target-user-id=1;tmi-sent-ts=2", "tmi.twitch.tv {0} #{2}", "{1}")]
+    // ban
+    [InlineData("CLEARCHAT", "testuser", "testchannel", true, "@room-id=0;target-user-id=1;tmi-sent-ts=2", "tmi.twitch.tv {0} #{2}", "{1}")]
+    //
+    [InlineData("MODE", "testuser", "testchannel", true, "", "{1}!{1}@{1}.tmi.twitch.tv {0} #{2}", "+o {1}")]
+    [InlineData("MODE", "testuser", "testchannel", true, "", "{1}!{1}@{1}.tmi.twitch.tv {0} #{2}", "-o {1}")]
+    //
+    [InlineData("USERSTATE", "testuser", "testchannel", true, "", "{1}!{1}@{1}.tmi.twitch.tv {0} #{2}", null)]
+    //
+    [InlineData("CLEARMSG", "testuser", "testchannel", true, "@login={1};room-id=;target-msg-id=some_msg_id_hash;tmi-sent-ts=1", "tmi.twitch.tv {0} #{2}", "some message")]
+    [InlineData("NOTICE", null, "testchannel", true, "@msg-id=msg_channel_suspended", "tmi.twitch.tv {0} #{2}", "This channel does not exist or has been suspended.")]
+    [InlineData("PRIVMSG", "testuser", "testchannel", true, "@badge-info=subscriber/36;badges=broadcaster/1,subscriber/3036,sub-gifter/5;client-nonce=hash;color=#B22222;display-name={1};emote-only=1;emotes=1:0-1;first-msg=0;flags=;id=some_msg_id_hash;mod=0;returning-chatter=0;room-id=0;subscriber=1;tmi-sent-ts=1;turbo=0;user-id=1;user-type=", "{1}!{1}@{1}.tmi.twitch.tv {0} #{2}", ":)")]
+    [InlineData("ROOMSTATE", null, "testchannel", true, "@emote-only=0;followers-only=0;r9k=0;room-id=1;slow=0;subs-only=0", "tmi.twitch.tv {0} #{2}", null)]
+    [InlineData("USERNOTICE", "testuser", "testchannel", true, "@badge-info=;badges=;color=#FF0000;display-name={1};emotes=;flags=;id=some_msg_id_hash;login={1};mod=0;msg-id=resub;msg-param-cumulative-months=5;msg-param-months=0;msg-param-multimonth-duration=0;msg-param-multimonth-tenure=0;msg-param-should-share-streak=1;msg-param-streak-months=1;msg-param-sub-plan-name=bla;msg-param-sub-plan=1000;msg-param-was-gifted=false;room-id=0;subscriber=1;system-msg={1}\\ssubscribed\\sat\\sTier\\s1.\\sThey've\\ssubscribed\\sfor\\s5\\smonths,\\scurrently\\son\\sa\\s1\\smonth\\sstreak!;tmi-sent-ts=1;user-id=0;user-type=", "tmi.twitch.tv {0} #{2}", null)]
+    public void ParseTest(string cmd, string user, string channel, bool tagMetaSeperator, string tags, string meta, string message)
+    {
+        string tagsPart = Format(tags, cmd, user, channel);
+        string metaPart = Format(meta, cmd, user, channel);
+        string messagePart = Format(message, cmd, user, channel);
+        StringBuilder ircBuilder = new StringBuilder();
+        ircBuilder.Append(tagsPart);
+        if (tagMetaSeperator)
+        {
+            ircBuilder.Append(" :");
+        }
+        ircBuilder.Append(metaPart);
+        if (!messagePart.IsNullOrEmptyOrWhitespace())
+        {
+            ircBuilder.Append(" :");
+            ircBuilder.Append(messagePart);
+        }
+
+        string irc = ircBuilder.ToString().Trim();
+        JObject ircMessage = IrcJsonParser.Parse(irc);
+        Assert.Equal(cmd, ircMessage.Value<string>("command"));
+        Assert.Equal(irc, ircMessage.Value<string>("irc"));
+        Assert.Equal(channel, ircMessage.Value<string>("channel"));
+        if (meta.Contains("{1}"))
+        {
+            Assert.Equal(user, ircMessage.Value<string>("user"));
+        }
+        Assert.Equal(messagePart, ircMessage.Value<string>("message"));
+
+    }
+    [Fact]
+    public void ParseTestDetailed()
+    {
+        string irc = "@badge-info=subscriber/22;badges=subscriber/18,bits/1000;client-nonce=a_hash;color=#1E90FF;display-name=testuser;emote-only=1;emotes=1:0-1,8-9/555555584:4-5;first-msg=0;flags=;id=msg_id_hash;mod=0;returning-chatter=0;room-id=0;subscriber=1;tmi-sent-ts=1678800000000;turbo=0;user-id=1;user-type= :testuser!testuser@testuser.tmi.twitch.tv PRIVMSG #testchannel ::)  <3  :)";
+        JObject ircMessage = IrcJsonParser.Parse(irc);
+        Assert.Equal("PRIVMSG", ircMessage.Value<string>("command"));
+        Assert.Equal("testuser", ircMessage.Value<string>("user"));
+        Assert.Equal("testchannel", ircMessage.Value<string>("channel"));
+        Assert.Equal(":)  <3  :)", ircMessage.Value<string>("message"));
+        Assert.Equal(irc, ircMessage.Value<string>("irc"));
+        //
+        Assert.NotNull(ircMessage["tags"]);
+        Assert.IsType<JObject>(ircMessage["tags"]);
+        JObject tags = (JObject) ircMessage["tags"];
+        Assert.NotNull(tags.Properties());
+        Assert.Equal(18, tags.Properties().Count());
+        Assert.Equal("a_hash", tags.Value<string>("client-nonce"));
+        Assert.Equal("#1E90FF", tags.Value<string>("color"));
+        Assert.Equal("testuser", tags.Value<string>("display-name"));
+        Assert.Equal("1", tags.Value<string>("emote-only"));
+        Assert.Equal("0", tags.Value<string>("first-msg"));
+        Assert.Equal("", tags.Value<string>("flags"));
+        Assert.Equal("msg_id_hash", tags.Value<string>("id"));
+        Assert.Equal("0", tags.Value<string>("mod"));
+        Assert.Equal("0", tags.Value<string>("returning-chatter"));
+        Assert.Equal("0", tags.Value<string>("room-id"));
+        Assert.Equal("1", tags.Value<string>("subscriber"));
+        Assert.Equal("1678800000000", tags.Value<string>("tmi-sent-ts"));
+        Assert.Equal("0", tags.Value<string>("turbo"));
+        Assert.Equal("1", tags.Value<string>("user-id"));
+        Assert.Equal("", tags.Value<string>("user-type"));
+        //
+        Assert.NotNull(tags["badge-info"]);
+        Assert.IsType<JObject>(tags["badge-info"]);
+        JObject badgeInfo = (JObject) tags["badge-info"];
+        Assert.NotNull(badgeInfo.Properties());
+        Assert.Single(badgeInfo.Properties());
+        Assert.Equal("22", badgeInfo.Value<string>("subscriber"));
+        //
+        Assert.NotNull(tags["badges"]);
+        Assert.IsType<JObject>(tags["badges"]);
+        JObject badges = (JObject) tags["badges"];
+        Assert.NotNull(badges.Properties());
+        Assert.Equal(2, badges.Properties().Count());
+        Assert.Equal("18", badges.Value<string>("subscriber"));
+        Assert.Equal("1000", badges.Value<string>("bits"));
+        //
+        Assert.NotNull(tags["emotes"]);
+        Assert.IsType<JObject>(tags["emotes"]);
+        JObject emotes = (JObject) tags["emotes"];
+        Assert.NotNull(emotes.Properties());
+        Assert.Equal(2, emotes.Properties().Count());
+        // emote-id 1
+        Assert.IsType<JArray>(emotes["1"]);
+        JArray emoteA = (JArray) emotes["1"];
+        Assert.Equal(2, emoteA.Count());
+        foreach (JToken indexToken in emoteA)
+        {
+            JObject index = (JObject) indexToken;
+            if (String.Equals("0", index.Value<string>("from")))
+            {
+                Assert.Equal("1", index.Value<string>("to"));
+                continue;
+            }
+            Assert.Equal("8", index.Value<string>("from"));
+            Assert.Equal("9", index.Value<string>("to"));
+        }
+        // emote-id 555555584
+        Assert.IsType<JArray>(emotes["555555584"]);
+        JArray emoteB = (JArray) emotes["555555584"];
+        Assert.Single(emoteB);
+        Assert.Equal("4", emoteB[0].Value<string>("from"));
+        Assert.Equal("5", emoteB[0].Value<string>("to"));
+        //
+    }
+    private static string Format(string stringToFormat, string cmd, string user, string channel)
+    {
+        if (stringToFormat == null)
+        {
+            return null;
+        }
+
+        return String.Format(stringToFormat, new string[] { cmd, user, channel });
+    }
+}
---
+++ TwitchLib.Client/Parsers/IrcJsonParser.cs
@@ -0,0 +1,242 @@
+﻿using System;
+
+using Newtonsoft.Json.Linq;
+
+using TwitchLib.Client.Extensions.Internal;
+
+namespace TwitchLib.Client.Parsers {
+    internal class IrcJsonParser {
+        /// <summary>
+        ///     parses the given <paramref name="irc"/>-message as <see cref="JObject"/>
+        /// </summary>
+        /// <param name="irc">
+        ///     raw irc-message as <see langword="string"/>
+        /// </param>
+        /// <returns>
+        ///     for easier access, almost everything is a <see cref="JObject"/> having <see cref="JObject"/>s as <see cref="JProperty"/>s
+        ///     <br></br>
+        ///     <br></br>
+        ///     <see cref="JObject.Properties()"/> are <see cref="IJEnumerable{T}"/> and can be accessed by their names, too, eg.:
+        ///     <code>
+        ///         ircObject["command"]
+        ///     </code>
+        ///     <code>
+        ///         ircObject.Value&lt;string&gt;("command")
+        ///     </code>
+        ///     <br></br>
+        ///     <br></br>
+        ///     only emote-indices are <see cref="JArray"/>s of <see cref="JObject"/>s having <see langword="from"/> and <see langword="to"/> as <see cref="JProperty"/>s
+        ///     <code>
+        ///         ircObject.Value&lt;JObject&gt;("tags").Value&lt;JObject&gt;("emotes").Value&lt;JArray&gt;("2")
+        ///     </code>
+        ///     <br></br>
+        ///     <br></br>
+        ///     tags, that are present within the raw irc, but have no value return an empty <see langword="string"/>
+        ///     <br></br>
+        ///     <br></br>
+        ///     example:
+        ///     <code>
+        ///     {
+        ///         "command": "irc command",
+        ///         "user": "user-login",
+        ///         "channel": "channel-name",
+        ///         "message": "the message",
+        ///         "irc": "the given raw irc-message",
+        ///         "tags": {
+        ///             "badge-info": {
+        ///                 "subscriber": "22"
+        ///             },
+        ///             "badges": {
+        ///                 "subscriber": "18",
+        ///                 "bits": "1000"
+        ///             },
+        ///             "emotes": {
+        ///                 "id of an emote, eg 1": [
+        ///                     {
+        ///                         "from": "1",
+        ///                         "to": "2"
+        ///                     }
+        ///                 ],
+        ///                 "id of an emote, eg 2": [
+        ///                     {
+        ///                         "from": "4",
+        ///                         "to": "5"
+        ///                     },
+        ///                     {
+        ///                         "from": "7",
+        ///                         "to": "8"
+        ///                     }
+        ///                 ]
+        ///             },
+        ///             "some tag name": "some tag value"
+        ///         }
+        ///     }
+        ///     </code>
+        /// </returns>
+        public static JObject Parse(string irc) {
+            JObject ircObject = new JObject();
+            bool hasTags = irc.StartsWith("@");
+            bool isPingOrPong = irc.StartsWith("PING") || irc.StartsWith("PONG");
+            string[] messageParts = GetMessagePartsFromRawIRC(irc);
+            string command = GetIRCCommand(messageParts, hasTags, isPingOrPong);
+            if (!command.IsNullOrEmptyOrWhitespace()) {
+                ircObject.Add(nameof(command), new JValue(command));
+            }
+            string user = GetUser(messageParts, command, hasTags, isPingOrPong);
+            if (!user.IsNullOrEmptyOrWhitespace()) {
+                ircObject.Add(nameof(user), new JValue(user));
+            }
+            string channel = GetChannel(messageParts, hasTags, isPingOrPong);
+            if (!channel.IsNullOrEmptyOrWhitespace()) {
+                ircObject.Add(nameof(channel), new JValue(channel));
+            }
+            string message = GetMessage(messageParts, hasTags, isPingOrPong);
+            if (!message.IsNullOrEmptyOrWhitespace()) {
+                ircObject.Add(nameof(message), new JValue(message));
+            }
+            if (hasTags) {
+                string tagPart = messageParts[0].Substring(1);
+
+                JObject tagObject = new JObject();
+
+                string[] tags = tagPart.Split(';');
+                foreach (string tag in tags) {
+                    string[] keyValue = tag.Split('=');
+
+                    string key = keyValue[0];
+                    string value = keyValue[1];
+                    JToken tagValue = new JValue("");
+                    if (String.Equals("emotes", key)) {
+                        if (!value.IsNullOrEmptyOrWhitespace()) {
+                            tagValue = GetEmotes(value);
+                        }
+                    } else if (key.StartsWith("badge")) {
+                        if (!value.IsNullOrEmptyOrWhitespace()) {
+                            tagValue = GetBadgeStuff(value);
+                        }
+                    } else {
+                        tagValue = new JValue(value);
+                    }
+
+                    tagObject.Add(key, tagValue);
+                }
+
+                ircObject.Add("tags", tagObject);
+            }
+
+            ircObject.Add(nameof(irc), irc);
+            return ircObject;
+        }
+
+        private static string GetMessage(string[] messageParts, bool hasTags, bool isPingOrPong) {
+            if (hasTags) {
+                if (messageParts.Length < 3) {
+                    return null;
+                }
+                return messageParts[2];
+            }
+            if (messageParts.Length < 2) {
+                return null;
+            }
+            return messageParts[1];
+        }
+
+        private static string GetChannel(string[] messageParts, bool hasTags, bool isPingOrPong) {
+            if (isPingOrPong) {
+                return null;
+            }
+            string meta;
+            if (hasTags) {
+                meta = messageParts[1];
+            } else {
+                meta = messageParts[0];
+            }
+            if (!meta.Contains("#")) {
+                return null;
+            }
+            return meta.Substring(meta.IndexOf("#") + 1);
+        }
+
+        private static string GetUser(string[] messageParts, string command, bool hasTags, bool isPingOrPong) {
+            if (isPingOrPong) {
+                return null;
+            }
+            string meta;
+            if (hasTags) {
+                meta = messageParts[1];
+            } else if (Int32.TryParse(command, out _)) {
+                meta = messageParts[0];
+                return meta.Split(' ')[2];
+            } else {
+                meta = messageParts[0];
+            }
+            string potentialUser = meta.Split(' ')[0];
+            if (!potentialUser.Contains("@")) {
+                return null;
+            }
+            return potentialUser.Split('@')[1].Split('.')[0];
+        }
+
+        private static string GetIRCCommand(string[] messageParts, bool hasTags, bool isPingOrPong) {
+            if (isPingOrPong) {
+                return messageParts[0];
+            }
+            string meta;
+            if (hasTags) {
+                meta = messageParts[1];
+            } else {
+                meta = messageParts[0];
+            }
+            return meta.Split(' ')[1];
+        }
+
+        private static JToken GetEmotes(string value) {
+            //emotes
+            //=
+            //1:0-1,8-9/555555584:4-5;
+            JObject emotesObject = new JObject();
+            string[] entries = value.Split('/');
+            foreach (string entry in entries) {
+                string[] idAndIndices = entry.Split(':');
+                string id = idAndIndices[0];
+                JArray indiceArray = new JArray();
+                string[] indices = idAndIndices[1].Split(',');
+                foreach (string index in indices) {
+                    JObject indexObject = new JObject();
+                    string[] fromTo = index.Split('-');
+                    indexObject.Add("from", fromTo[0]);
+                    indexObject.Add("to", fromTo[1]);
+                    indiceArray.Add(indexObject);
+                }
+                emotesObject.Add(id, indiceArray);
+            }
+            return emotesObject;
+        }
+
+        private static JToken GetBadgeStuff(string raw) {
+            string[] entries = raw.Split(',');
+            JObject badgeObject = new JObject();
+            foreach (string entry in entries) {
+                if (entry.IsNullOrEmptyOrWhitespace()) {
+                    continue;
+                }
+                string[] keyValuePair = entry.Split('/');
+                badgeObject.Add(keyValuePair[0], new JValue(keyValuePair[1]));
+            }
+            return badgeObject;
+        }
+
+        private static string[] GetMessagePartsFromRawIRC(string rawIrc) {
+            string[] messageParts = rawIrc.Split(new string[] { " :" }, StringSplitOptions.None);
+            if (messageParts.Length > 3) {
+                messageParts = new string[]
+                {
+                    messageParts[0],
+                    messageParts[1],
+                    messageParts[2] = String.Join(" :", messageParts, 2, messageParts.Length-2),
+                };
+            }
+            return messageParts;
+        }
+    }
+}
