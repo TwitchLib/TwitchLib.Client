08466f8cf4c3a494ce0d3aa07e93825ac6de0a9b
CMR <github.christian-riedel@outlook.de>
2023-03-24 17:30:43 +0100

    tests

--- TwitchLib.Client.Tests/Models/JoinedChannelTests.cs
+++ TwitchLib.Client.Tests/Models/JoinedChannelTests.cs
@@ -12,7 +12,6 @@ namespace TwitchLib.Client.Tests.Models
     public class JoinedChannelTests
     {
         private static string Channel => "testchannel";
-        private static string Username => "testusername";
         private static string MessageIdHash => "msg_id_hash";
         /// <summary>
         ///     only tests handling
@@ -24,20 +23,20 @@ namespace TwitchLib.Client.Tests.Models
         ///     <br></br>
         ///     <seealso cref="TwitchClient_MessageSendingTests.TwitchClient_Raises_OnMessageSent"/>
         /// </summary>
-        [Fact]
-        public void UserState_And_SentMessage_Test()
+        [Theory]
+        [InlineData("botusername", "testusername", 0)]
+        [InlineData("botusername", "botusername", 1)]
+        public void UserState_And_SentMessage_Test(string botusername, string senderUsername, int expectedSize)
         {
             ITwitchClient client = new TwitchClient();
-            JoinedChannel joinedChannel = new JoinedChannel(Channel, Username);
-            ChatMessage chatMessage = GetChatMessage();
+            JoinedChannel joinedChannel = new JoinedChannel(Channel, botusername);
+            ChatMessage chatMessage = GetChatMessage(senderUsername);
             joinedChannel.HandlePRIVMSG(chatMessage);
-            Assert.NotEmpty(joinedChannel.UnRaisedSentMessages);
-            Assert.Single(joinedChannel.UnRaisedSentMessages);
-            UserState userStateJoin = GetUserState(true);
+            Assert.Equal(expectedSize, joinedChannel.UnRaisedSentMessages.Count);
+            UserState userStateJoin = GetUserState(true, senderUsername);
             joinedChannel.HandleUSERSTATE(userStateJoin, client);
-            Assert.NotEmpty(joinedChannel.UnRaisedSentMessages);
-            Assert.Single(joinedChannel.UnRaisedSentMessages);
-            UserState userStateSent = GetUserState(false);
+            Assert.Equal(expectedSize, joinedChannel.UnRaisedSentMessages.Count);
+            UserState userStateSent = GetUserState(false, senderUsername);
             joinedChannel.HandleUSERSTATE(userStateSent, client);
             Assert.Empty(joinedChannel.UnRaisedSentMessages);
         }
@@ -86,9 +85,9 @@ namespace TwitchLib.Client.Tests.Models
             Assert.Equal(roomId, channelState.RoomId);
             Assert.True(channelState.EmoteOnly);
         }
-        private static UserState GetUserState(bool join)
+        private static UserState GetUserState(bool join, string senderUsername)
         {
-            string irc = $":{Username}!{Username}@{Username}.tmi.twitch.tv USERSTATE #{Channel}";
+            string irc = $":{senderUsername}!{senderUsername}@{senderUsername}.tmi.twitch.tv USERSTATE #{Channel}";
             if (!join)
             {
                 irc = $"@id={MessageIdHash} " + irc;
@@ -97,11 +96,11 @@ namespace TwitchLib.Client.Tests.Models
             UserState userState = new UserState(ircMessage);
             return userState;
         }
-        private static ChatMessage GetChatMessage()
+        private static ChatMessage GetChatMessage(string senderUsername)
         {
-            string irc = $"@badge-info=subscriber/22;badges=subscriber/18,bits/1000;client-nonce=a_hash;color=#1E90FF;display-name=testuser;emote-only=1;emotes=1:0-1,8-9/555555584:4-5;first-msg=0;flags=;id={MessageIdHash};mod=0;returning-chatter=0;room-id=0;subscriber=1;tmi-sent-ts=1678800000000;turbo=0;user-id=1;user-type= :{Username}!{Username}@{Username}.tmi.twitch.tv PRIVMSG #{Channel} ::)  <3  :)";
+            string irc = $"@badge-info=subscriber/22;badges=subscriber/18,bits/1000;client-nonce=a_hash;color=#1E90FF;display-name=testuser;emote-only=1;emotes=1:0-1,8-9/555555584:4-5;first-msg=0;flags=;id={MessageIdHash};mod=0;returning-chatter=0;room-id=0;subscriber=1;tmi-sent-ts=1678800000000;turbo=0;user-id=1;user-type= :{senderUsername}!{senderUsername}@{senderUsername}.tmi.twitch.tv PRIVMSG #{Channel} ::)  <3  :)";
             IrcMessage ircMessage = IrcParser.ParseIrcMessage(irc);
-            ChatMessage chatMessage = new ChatMessage(Username, ircMessage);
+            ChatMessage chatMessage = new ChatMessage(senderUsername, ircMessage);
             return chatMessage;
         }
     }
