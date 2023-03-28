95bec21364c5ecb04a510ff9620d172c3d94f458
CMR <github.christian-riedel@outlook.de>
2023-03-28 16:53:32 +0200

    docs and formatting

--- TwitchLib.Client/Exceptions/ClientNotConnectedException.cs
+++ TwitchLib.Client/Exceptions/ClientNotConnectedException.cs
@@ -3,21 +3,17 @@
 namespace TwitchLib.Client.Exceptions
 {
     /// <summary>
-    /// Exception thrown when attempting to perform an actino that is only available when the client is connected.
-    /// Implements the <see cref="System.Exception" />
+    ///     Exception thrown when attempting to perform an actino that is only available when the client is connected.
+    ///     Implements the <see cref="System.Exception" />
     /// </summary>
-    /// <seealso cref="System.Exception" />
-    /// <inheritdoc />
     public class ClientNotConnectedException : Exception
     {
         /// <summary>
-        /// Exception constructor
+        ///     Exception constructor
         /// </summary>
-        /// <param name="description">The description.</param>
-        /// <inheritdoc />
-        public ClientNotConnectedException(string description)
-            : base(description)
-        {
-        }
+        /// <param name="description">
+        ///     The description.
+        /// </param>
+        public ClientNotConnectedException(string description) : base(description) { }
     }
 }
