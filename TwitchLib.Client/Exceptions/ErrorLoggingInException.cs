d228c9a394c29b8a688d641c407b612e80660644
CMR <github.christian-riedel@outlook.de>
2023-03-28 16:53:17 +0200

    APIReference and ToString()

--- TwitchLib.Client/Exceptions/ErrorLoggingInException.cs
+++ TwitchLib.Client/Exceptions/ErrorLoggingInException.cs
@@ -1,31 +1,43 @@
 ﻿using System;
+using System.Text;
 
 namespace TwitchLib.Client.Exceptions
 {
     /// <summary>
-    /// Exception representing credentials provided for logging in were bad.
-    /// Implements the <see cref="System.Exception" />
+    ///     Exception representing credentials provided for logging in were bad.
+    ///     Implements the <see cref="System.Exception" />
     /// </summary>
-    /// <seealso cref="System.Exception" />
-    /// <inheritdoc />
     public class ErrorLoggingInException : Exception
     {
         /// <summary>
-        /// Exception representing username associated with bad login.
+        ///     Reference to TwitchAPI
         /// </summary>
-        /// <value>The username.</value>
+        public string APIReference => "https://dev.twitch.tv/docs/irc/authenticate-bot/";
+        /// <summary>
+        ///     Exception representing username associated with bad login.
+        /// </summary>
         public string Username { get; protected set; }
 
         /// <summary>
-        /// Exception construtor.
+        ///     Exception construtor.
         /// </summary>
-        /// <param name="ircData">The irc data.</param>
-        /// <param name="twitchUsername">The twitch username.</param>
-        /// <inheritdoc />
-        public ErrorLoggingInException(string ircData, string twitchUsername)
-            : base(ircData)
+        /// <param name="ircData">
+        ///     The irc data.
+        /// </param>
+        /// <param name="twitchUsername">
+        ///     The twitch username.
+        /// </param>
+        public ErrorLoggingInException(string ircData, string twitchUsername) : base(ircData)
         {
             Username = twitchUsername;
+        }
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine($"{nameof(Username)}: {Username}");
+            builder.AppendLine($"{nameof(Message)}: {Message}");
+            builder.AppendLine($"{nameof(APIReference)}: {APIReference}");
+            return builder.ToString();
         }
     }
 }
