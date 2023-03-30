99392e868ccc3b5f99aec2f7ade5039bce3af94d
CMR <54452101+christian-riedel@users.noreply.github.com>
2023-03-31 00:21:11 +0200

    some more preparation

---
+++ TwitchLib.Client.Tests.RealWorld/ClientRunnerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Interfaces;
+using TwitchLib.Client.Models;
+
+using Xunit;
+
+namespace TwitchLib.Client.Tests.RealWorld
+{
+    public class ClientRunnerTest
+    {
+        private string TwitchUsername => throw new NotImplementedException();
+        private string TwitchOAuth => throw new NotImplementedException();
+        private IEnumerable<string> Channels => throw new NotImplementedException();
+        [Fact]
+        public void Run()
+        {
+            ConnectionCredentials credentials = new ConnectionCredentials(TwitchUsername, TwitchOAuth);
+            ITwitchClient twitchClient = new TwitchClient(credentials);
+            twitchClient.JoinChannels(Channels);
+            twitchClient.OnConnected += OnConnected;
+            twitchClient.OnDisconnected += OnDisconnected;
+            twitchClient.OnReconnected += OnReconnected;
+            twitchClient.OnJoinedChannel += OnJoinedChannel;
+            twitchClient.OnUserJoined += OnUserJoined;
+            twitchClient.Connect();
+            // TODO: assertions...
+        }
+        private void OnConnected(object? sender, OnConnectedArgs args) { }
+        private void OnDisconnected(object? sender, OnDisconnectedArgs args) { }
+        private void OnReconnected(object? sender, OnConnectedArgs args) { }
+        private void OnJoinedChannel(object? sender, OnJoinedChannelArgs args) { }
+        private void OnUserJoined(object? sender, OnUserJoinedArgs args) { }
+
+    }
+}
