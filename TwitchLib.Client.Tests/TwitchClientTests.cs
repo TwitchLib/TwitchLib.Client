088056fefc4d1a1ff9f5dec9fe0f46e81d789b5b
CMR <54452101+christian-riedel@users.noreply.github.com>
2023-03-31 00:06:36 +0200

    tests

---
+++ TwitchLib.Client.Tests/TwitchClientTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using TwitchLib.Client.Interfaces;
+using TwitchLib.Client.Models;
+
+using Xunit;
+
+namespace TwitchLib.Client.Tests
+{
+    public class TwitchClientTests : ATwitchClientTests<ITwitchClient>
+    {
+        [Fact]
+        public void ConstructorTest()
+        {
+            try
+            {
+                ITwitchClient client = new TwitchClient(null);
+                Assert.Fail($"{typeof(ArgumentNullException)} expected!");
+            }
+            catch (Exception e)
+            {
+                Assert.NotNull(e);
+                Assert.IsType<ArgumentNullException>(e);
+            }
+        }
+        [Fact]
+        public void ChatCommandIdentifierTest()
+        {
+            ConnectionCredentials credentials = new ConnectionCredentials(TWITCH_Username, TWITCH_OAuth);
+            ITwitchClient client = new TwitchClient(credentials);
+            CheckChatCommandIdentifiers(client, 1, new[] { '!' });
+            client.RemoveChatCommandIdentifier('!');
+            CheckChatCommandIdentifiers(client, 0, Array.Empty<char>());
+            client.AddChatCommandIdentifier('!');
+            CheckChatCommandIdentifiers(client, 1, new[] { '!' });
+            client.AddChatCommandIdentifier('!');
+            CheckChatCommandIdentifiers(client, 1, new[] { '!' });
+            client.AddChatCommandIdentifier('/');
+            CheckChatCommandIdentifiers(client, 2, new[] { '!', '/' });
+        }
+        private void CheckChatCommandIdentifiers(ITwitchClient client, int expectedSize, char[] expectedContent)
+        {
+            TypeInfo typeInfo = client.GetType().GetTypeInfo();
+            PropertyInfo? propertyInfo = typeInfo.GetDeclaredProperty("ChatCommandIdentifiers");
+            Assert.NotNull(propertyInfo);
+            object? propertyValue = propertyInfo.GetValue(client);
+            Assert.NotNull(propertyValue);
+            ISet<char> chatCommandIdentifiers = Assert.IsAssignableFrom<ISet<char>>(propertyValue);
+            Assert.Equal(expectedSize, chatCommandIdentifiers.Count);
+            Assert.Equal<IEnumerable<char>>(expectedContent, chatCommandIdentifiers);
+        }
+    }
+}
