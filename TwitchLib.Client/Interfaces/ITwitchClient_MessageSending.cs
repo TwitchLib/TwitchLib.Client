9af54e8a7f2b1cc4176a12be4c2acb204861619a
CMR <54452101+christian-riedel@users.noreply.github.com>
2023-04-01 16:28:03 +0200

    docs

--- TwitchLib.Client/Interfaces/ITwitchClient_MessageSending.cs
+++ TwitchLib.Client/Interfaces/ITwitchClient_MessageSending.cs
@@ -29,39 +29,72 @@ namespace TwitchLib.Client.Interfaces
 
         #region methods public
         /// <summary>
-        ///     Sends the message.
+        ///     Sends the given <paramref name="message"/> in the given <paramref name="channel"/>
+        ///     <br></br>
+        ///     <b>take care</b>,
+        ///     <br></br>
+        ///     if the message has more than 500 <see langword="char"/>s,
+        ///     <br></br>
+        ///     it wont get sent
         /// </summary>
-        /// <param name="channel">The channel.</param>
-        /// <param name="message">The message.</param>
-        /// <param name="dryRun">if set to <c>true</c> [dry run].</param>
+        /// <param name="channel">
+        ///     channel-name
+        /// </param>
+        /// <param name="message">
+        ///     'normal' message to send
+        ///     <br></br>
+        ///     <b>no</b> raw irc-message
+        ///     <br></br>
+        ///     therefore use <see cref="SendRaw(String)"/> instead
+        /// </param>
+        /// <param name="dryRun">
+        ///     if set to <see langword="true"/>, the created <see cref="OutboundChatMessage"/> wont be added to send-queue
+        /// </param>
         void SendMessage(JoinedChannel channel, string message, bool dryRun = false);
-        /// <summary>
-        ///     Sends the message.
-        /// </summary>
-        /// <param name="channel">The channel.</param>
-        /// <param name="message">The message.</param>
-        /// <param name="dryRun">if set to <c>true</c> [dry run].</param>
+        /// <inheritdoc cref="SendMessage(JoinedChannel, String, Boolean)"/>
         void SendMessage(string channel, string message, bool dryRun = false);
         /// <summary>
-        ///     Sends a formatted Twitch chat message reply.
+        ///     Sends the given <paramref name="message"/> in the given <paramref name="channel"/>
+        ///     as reply to a message with the given <paramref name="replyToId"/>
+        ///     <br></br>
+        ///     <b>take care</b>,
+        ///     <br></br>
+        ///     if the message has more than 500 <see langword="char"/>s,
+        ///     <br></br>
+        ///     it wont get sent
         /// </summary>
-        /// <param name="channel">Channel to send Twitch chat reply to</param>
-        /// <param name="replyToId">The message id that is being replied to</param>
-        /// <param name="message">Reply contents</param>
-        /// <param name="dryRun">if set to <c>true</c> [dry run]</param>
+        /// <param name="channel">
+        ///     channel-name
+        /// </param>
+        /// <param name="replyToId">
+        ///     <inheritdoc cref="ChatMessage.Id"/>
+        ///     see also
+        ///     <br></br>
+        ///     <see cref="ChatMessage.Id"/>
+        /// </param>
+        /// <param name="message">
+        ///     'normal' message to send
+        ///     <br></br>
+        ///     <b>no</b> raw irc-message
+        ///     <br></br>
+        ///     therefore use <see cref="SendRaw(String)"/> instead
+        /// </param>
+        /// <param name="dryRun">
+        ///     if set to <see langword="true"/>, the created <see cref="OutboundChatMessage"/> wont be added to send-queue
+        /// </param>
         void SendReply(JoinedChannel channel, string replyToId, string message, bool dryRun = false);
-        /// <summary>
-        ///     SendReply wrapper that accepts channel in string form.
-        /// </summary>
-        /// <param name="channel">Channel to send Twitch chat reply to</param>
-        /// <param name="replyToId">The message id that is being replied to</param>
-        /// <param name="message">Reply contents</param>
-        /// <param name="dryRun">if set to <c>true</c> [dry run]</param>
+        /// <inheritdoc cref="SendReply(JoinedChannel, String, String, Boolean)"/>
         void SendReply(string channel, string replyToId, string message, bool dryRun = false);
         /// <summary>
-        ///     Sends the raw.
+        ///     sends an raw irc-message
+        ///     <br></br>
+        ///     <b>take care</b>,
+        ///     <br></br>
+        ///     it bypasses the integrated <see cref="Services.ThrottlerService"/>
         /// </summary>
-        /// <param name="message">The message.</param>
+        /// <param name="message">
+        ///     an raw irc-message
+        /// </param>
         void SendRaw(string message);
         #endregion methods public
     }
