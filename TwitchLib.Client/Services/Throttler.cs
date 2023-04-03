fb50c829a54ce7b5af7d2388a2031e175efc2ebe
CMR <54452101+christian-riedel@users.noreply.github.com>
2023-04-03 10:30:43 +0200

    init first with null and added docs and ref

--- TwitchLib.Client/Services/Throttler.cs
+++ TwitchLib.Client/Services/Throttler.cs
@@ -5,7 +5,62 @@ using TwitchLib.Client.Models.Interfaces;
 namespace TwitchLib.Client.Services {
     internal class Throttler {
         private ISendOptions SendOptions { get; }
-        private DateTime First { get; set; } = DateTime.UtcNow;
+        /// <summary>
+        ///     <see cref="DateTime"/> of the first message
+        ///     <br></br>
+        ///     to determine, when <see cref="ISendOptions.ThrottlingPeriod"/> begins
+        ///     <br></br>
+        ///     <br></br>
+        ///     has to be initialized with <see langword="null"/>
+        ///     <br></br>
+        ///     reason:
+        ///     <list type="number">
+        ///         <item>
+        ///             <see cref="Throttler"/> gets instantiated and <see cref="First"/> gets initialized
+        ///         </item>
+        ///         <item>
+        ///             <see cref="ISendOptions.ThrottlingPeriod"/> is always set to 30 seconds
+        ///         </item>
+        ///         <item>
+        ///             start sending messages after 20 seconds after 'step 1' and hitting <see cref="ISendOptions.SendsAllowedInPeriod"/> (<see cref="Enums.MessageRateLimit"/>)
+        ///         </item>
+        ///         <item>
+        ///             we dont take care about it and dont stop sending messages
+        ///         </item>
+        ///         <item>
+        ///             <see cref="ISendOptions.ThrottlingPeriod"/> gets exceeded
+        ///         </item>
+        ///         <item>
+        ///             <see cref="Reset"/> is called
+        ///         </item>
+        ///         <item>
+        ///             but we still hit the Rate-Limit,
+        ///             now server-side,
+        ///             cause <see cref="ISendOptions.ThrottlingPeriod"/>
+        ///             <br></br>
+        ///             starts at 'step 1 (second 0) + step 3 (20 seconds)'
+        ///             <br></br>
+        ///             and ends at 'step 1 (second 0) + step 3 (20 seconds) + <see cref="ISendOptions.ThrottlingPeriod"/> (30 secconds)'
+        ///             <br></br>
+        ///             in other words: at second 50 calculated from step 1
+        ///         </item>
+        ///     </list>
+        ///     <see href="https://dev.twitch.tv/docs/irc/#rate-limits"/>
+        ///     <br></br>
+        ///     1. paragraph:
+        ///     <br></br>
+        ///     The Twitch IRC server enforces the following limits.
+        ///     It is up to your bot to keep track of its usage and not exceed the limits.
+        ///     Rate limit counters begin when the server processes the first message and resets at the end of the window.
+        ///     For example,
+        ///     if the limit is 20 messages per 30 seconds,
+        ///     the window starts when the server processes the first message
+        ///     and lasts for 30 seconds.
+        ///     At the end of the window,
+        ///     the counter resets
+        ///     and a new window begins with the next message.
+        /// </summary>
+        private DateTime? First { get; set; } = null;
         private uint SentItemCount { get; set; } = 0;
         public Throttler(ISendOptions sendOptions) {
             this.SendOptions = sendOptions;
@@ -49,7 +104,8 @@ namespace TwitchLib.Client.Services {
         ///     <see langword="false"/> otherwise
         /// </returns>
         private bool IsThrottlingPeriodExceeded() {
-            return DateTime.UtcNow.Subtract(this.First) > this.SendOptions.ThrottlingPeriod;
+            return this.First == null
+                   || DateTime.UtcNow.Subtract((DateTime) this.First) > this.SendOptions.ThrottlingPeriod;
         }
     }
 }
